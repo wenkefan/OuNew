041adc16440e4f72576dc24840eb670f5bbbfa62
fanwenke <wenfwk@163.com>
2017-08-07 23:44:48 +0800

    activity_news关联content_news时报错

--- app/src/main/java/com/wenke/ounew/module/news/ui/NewsActivity.java
+++ app/src/main/java/com/wenke/ounew/module/news/ui/NewsActivity.java
@@ -1,20 +1,18 @@
 package com.wenke.ounew.module.news.ui;
 
-import android.os.Bundle;
-
 import com.wenke.ounew.R;
 import com.wenke.ounew.annotation.ActivityFragmentInject;
 import com.wenke.ounew.base.BaseActivity;
 
 @ActivityFragmentInject(
-        contentViewId = R.layout.activity_main
+        contentViewId = R.layout.activity_news
 )
 public class NewsActivity extends BaseActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//    }
 
     @Override
     protected void initView() {
